5150ca00da505eb1bcbab20a67cff547c2347b93
Rui-Nagatsu <142547550+Rui-Nagatsu@users.noreply.github.com>
2023-11-30 23:44:52 +0000

    sabado

---
+++ app/js/eventos.js
@@ -0,0 +1,10 @@
+// console.log("funciona!!!");
+
+// let el = document.querySelector('#txtExemplo');
+
+// el.addEventListener('focusout', function(e) {
+   // console.log(e.target.value);
+  // document.querySelector('#txtClone').value = e.target.value
+// });
+
+// console.log(el);
